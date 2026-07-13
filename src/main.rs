272435399d5fb4afa4042dfa927e5e46927ec11d
CodesOtakuYT <taouaouilyas@gmail.com>
2026-07-13 02:34:41 +0100

    cube

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,84 @@
+use bytemuck::{Pod, Zeroable};
+use std::f64::consts;
 use std::sync::Arc;
+use wgpu::util::{BufferInitDescriptor, DeviceExt};
 use wgpu::wgt::{CommandEncoderDescriptor, TextureViewDescriptor};
 use wgpu::{include_wgsl, InstanceDescriptor, SurfaceConfiguration};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowAttributes, WindowId};
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct Vertex {
+    _pos: [f32; 4],
+    _tex_coord: [f32; 2],
+}
+
+fn vertex(pos: [i8; 3], tc: [i8; 2]) -> Vertex {
+    Vertex {
+        _pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0],
+        _tex_coord: [tc[0] as f32, tc[1] as f32],
+    }
+}
+
+fn create_vertices() -> (Vec<Vertex>, Vec<u16>) {
+    let vertex_data = [
+        // top (0, 0, 1)
+        vertex([-1, -1, 1], [0, 0]),
+        vertex([1, -1, 1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([-1, 1, 1], [0, 1]),
+        // bottom (0, 0, -1)
+        vertex([-1, 1, -1], [1, 0]),
+        vertex([1, 1, -1], [0, 0]),
+        vertex([1, -1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // right (1, 0, 0)
+        vertex([1, -1, -1], [0, 0]),
+        vertex([1, 1, -1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([1, -1, 1], [0, 1]),
+        // left (-1, 0, 0)
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, 1, 1], [0, 0]),
+        vertex([-1, 1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // front (0, 1, 0)
+        vertex([1, 1, -1], [1, 0]),
+        vertex([-1, 1, -1], [0, 0]),
+        vertex([-1, 1, 1], [0, 1]),
+        vertex([1, 1, 1], [1, 1]),
+        // back (0, -1, 0)
+        vertex([1, -1, 1], [0, 0]),
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, -1, -1], [1, 1]),
+        vertex([1, -1, -1], [0, 1]),
+    ];
+
+    let index_data: &[u16] = &[
+        0, 1, 2, 2, 3, 0, // top
+        4, 5, 6, 6, 7, 4, // bottom
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
+
+    (vertex_data.to_vec(), index_data.to_vec())
+}
+
+fn camera_matrix(aspect_ratio: f64) -> glamx::DMat4 {
+    let projection =
+        glamx::dcamera::rh::proj::directx::perspective(consts::FRAC_PI_4, aspect_ratio, 1.0, 10.0);
+    let view = glamx::dcamera::rh::view::look_at_mat4(
+        glamx::DVec3::new(1.5, -5.0, 3.0),
+        glamx::DVec3::ZERO,
+        glamx::DVec3::Z,
+    );
+    projection * view
+}
 
 struct State {
     window: Arc<Window>,
@@ -15,6 +89,11 @@ struct State {
     surface_format: wgpu::TextureFormat,
     surface: Option<wgpu::Surface<'static>>,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    uniform_buffer: wgpu::Buffer,
+    index_count: u32,
+    bind_group: wgpu::BindGroup,
 }
 
 impl State {
@@ -43,16 +122,60 @@ impl State {
         let surface_capabilities = temporary_surface.get_capabilities(&adapter);
         let surface_format = surface_capabilities.formats[0];
 
+        let (window_width, window_height): (u32, u32) = window.inner_size().into();
+        let matrix = camera_matrix(window_width as f64 / window_height as f64).as_mat4();
+        let uniform_buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: None,
+            contents: bytemuck::cast_slice(matrix.as_ref()),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: None,
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: wgpu::BufferSize::new(64),
+                },
+                count: None,
+            }],
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[Some(&bind_group_layout)],
+            immediate_size: 0,
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            }],
+        });
+
         let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
 
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: None,
-            layout: None,
+            layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
                 compilation_options: Default::default(),
-                buffers: &[],
+                buffers: &[Some(wgpu::VertexBufferLayout {
+                    array_stride: size_of::<Vertex>() as wgpu::BufferAddress,
+                    step_mode: wgpu::VertexStepMode::Vertex,
+                    attributes: &wgpu::vertex_attr_array![
+                        0 => Float32x4,
+                        1 => Float32x2,
+                    ],
+                })],
             },
             primitive: Default::default(),
             depth_stencil: None,
@@ -67,6 +190,20 @@ impl State {
             cache: None,
         });
 
+        let (vertices, indices) = create_vertices();
+
+        let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(&vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(&indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
         Ok(Self {
             window,
             instance,
@@ -76,6 +213,11 @@ impl State {
             surface_format,
             surface: None,
             render_pipeline,
+            vertex_buffer,
+            index_buffer,
+            uniform_buffer,
+            index_count: indices.len() as u32,
+            bind_group,
         })
     }
 
@@ -103,6 +245,13 @@ impl State {
                     alpha_mode: wgpu::CompositeAlphaMode::Auto,
                     view_formats: vec![self.surface_format.add_srgb_suffix()],
                 },
+            );
+            let (window_width, window_height): (u32, u32) = self.window.inner_size().into();
+            let matrix = camera_matrix(window_width as f64 / window_height as f64).as_mat4();
+            self.queue.write_buffer(
+                &self.uniform_buffer,
+                0,
+                bytemuck::cast_slice(matrix.as_ref()),
             );
         }
     }
@@ -162,7 +311,10 @@ impl State {
                     ..Default::default()
                 });
                 renderpass.set_pipeline(&self.render_pipeline);
-                renderpass.draw(0..3, 0..1);
+                renderpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+                renderpass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                renderpass.set_bind_group(0, &self.bind_group, &[]);
+                renderpass.draw_indexed(0..self.index_count, 0, 0..1);
             }
 
             self.queue.submit(std::iter::once(encoder.finish()));
